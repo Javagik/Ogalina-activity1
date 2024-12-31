d22f33e7e8cc04c6782b27896cf0726e1bdd2bb7
Javagik <aronogalina1@gmail.com>
2024-12-31 12:42:26 +0800

    commit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post, Publisher, Category, Score, Activity, UserProfile
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Publisher)
+admin.site.register(Category)
+admin.site.register(Score)
+admin.site.register(Activity)
+admin.site.register(UserProfile)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+      return self.title
+
+    
+
+class Publisher(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+     return reverse('blog_detail', kwargs={'pk': self.pk})
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Score(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    categories = models.ManyToManyField(Category, related_name='Score') 
+
+    def __str__(self):
+        return self.name
+
+class Activity(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Progress', 'Progress'),
+        ('Done', 'DOne'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Progress') 
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+
+    def __str__(self):
+        return f"Progress {self.id} by {self.user.username}"
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    address = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
